579eeba44ef02f2d77cfaf60306ef20f7cbd1a00
kthuytrang <kimberlyoliviatran@gmail.com>
2024-05-27 21:55:18 -0400

    i hate bingo
    i never want to see another bingo card again.

--- script.js
+++ script.js
@@ -18,11 +18,20 @@
 
             // Add extra row for "BINGO"
             var headerRow = document.createElement("tr");
+            headerRow.classList.add("header");
             var headerCell = document.createElement("th");
+            var topLeft = document.createElement("img");
+            topLeft.src="images/rainbowLogo.png";
+            headerRow.appendChild(topLeft);
             headerCell.setAttribute("colspan", "5");
-            headerCell.textContent = "BINGO";
+            headerCell.textContent = "B I N G O";
             headerRow.appendChild(headerCell);
-            table.appendChild(headerRow);
+            var topRight = document.createElement("img");
+            topRight.src="images/rainbowLogo.png";
+            headerRow.appendChild(topRight);
+            cardContainer.appendChild(headerRow);
+
+            
 
             var cellIndex = 0;
 
@@ -37,19 +46,24 @@
         		// Center cell is an image
         		if (i === 2 && j === 2) {
             		var imgElement = document.createElement("img");
-           			imgElement.src = "images/rainbowImage.jpg"; // Replace with your image URL or Base64 data
+           			imgElement.src = "images/rainbowCenter.jpg"; // Replace with your image URL or Base64 data
             		imgElement.style.maxWidth = "100%";
             		imgElement.style.maxHeight = "90%";
-					imgElement.style.paddingTop = "10%";
            		 	cell.appendChild(imgElement);
         		} else {
             		cell.textContent = numbers[cellIndex++];
+                    
         }
 
         row.appendChild(cell);
+
+
+        
     }
 
     table.appendChild(row);
+
+    
 }
 
 // ...
@@ -94,11 +108,19 @@
             generatedCards.push(numbers);
             return numbers;
         }
+        var footerRow = document.createElement("tr");
+            var footerCell = document.createElement("th");
+            footerRow.classList.add("adhc-footer");
+            footerCell.setAttribute("colspan", "1");
+            footerCell.textContext = "RAINBOW ADULT DAY HEALTH";
+            footerRow.appendChild(footerCell);
+            cardContainer.appendChild(footerRow);
     }
 
-    document.getElementById('numCards').addEventListener('keyup', function(event){
-        if (event.key==="Enter"){
-            console.log("hi :)");
-            generateBingoCards();
-        }
-    });
+
+document.getElementById('numCards').addEventListener('keyup', function(event){
+    if (event.key==="Enter"){
+        console.log("hi :)");
+        generateBingoCards();
+    }
+});
